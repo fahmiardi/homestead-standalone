f195631337fb5869e67dcd067206eaf119394848
Fahmi Ardi <f4hem.net@gmail.com>
2017-10-17 16:11:00 +0700

    support use downloaded box file

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -14,8 +14,9 @@ class Homestead
 
         # Configure The Box
         config.vm.define settings["name"] ||= "homestead-7"
-        config.vm.box = settings["box"] ||= "laravel/homestead"
-        config.vm.box_version = settings["version"] ||= ">= 3.0.0"
+        config.vm.box = settings["box"] ||= "homestead"
+        config.vm.box_url = settings["box_url"] ||= "laravel/homestead"
+        # config.vm.box_version = settings["version"] ||= ">= 3.0.0"
         config.vm.hostname = settings["hostname"] ||= "homestead"
 
         # Configure A Private Network IP
